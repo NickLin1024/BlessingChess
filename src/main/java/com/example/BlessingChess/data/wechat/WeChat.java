7e7f5bf36e6d8677159e137bba42088a324853b6
NickLin1024 <152240432+NickLin1024@users.noreply.github.com>
2024-03-02 01:36:09 +0800

    JOKER
    哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈

--- src/main/java/com/example/BlessingChess/data/wechat/WeChat.java
+++ src/main/java/com/example/BlessingChess/data/wechat/WeChat.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class WeChat {
-    public static String appId;
-    public static String secret;
+    public static String appId = "wx3d2157da22a2c62a";
+    public static String secret = "2e783414161799d735b92d4fe4386c95";
 }
